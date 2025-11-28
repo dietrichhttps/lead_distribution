40bc0452b8fb7d3b9ee4725fa8f170dfb26d1ff5
Ali Abaikhanov <abayhanov1998@gmail.com>
2025-11-28 23:31:03 +0300

    Changed tables creating logic

--- app/database.py
+++ app/database.py
@@ -5,3 +5,10 @@ SQLITE_DATABASE_URL = "sqlite:///./leads.db"
 engine = create_engine(SQLITE_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+def create_tables():
+    try:
+        Base.metadata.create_all(bind=engine)
+        print("✅ Таблицы успешно созданы")
+    except Exception as e:
+        print(f"❌ Ошибка создания таблиц: {e}")
--- app/main.py
+++ app/main.py
@@ -1,8 +1,4 @@
-from app.api.api import app
-import uvicorn
-
-from app.database import Base, engine
+from app.database import create_tables
 
 if __name__ == "__main__":
-    Base.metadata.create_all(bind=engine)
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    create_tables()
