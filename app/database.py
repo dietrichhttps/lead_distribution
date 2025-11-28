6bc21b4d58e015ed797a76ebc7610ff4736642e9
Ali Abaikhanov <abayhanov1998@gmail.com>
2025-11-28 23:25:21 +0300

    Changed tables creating logic

--- app/database.py
+++ app/database.py
@@ -5,4 +5,3 @@ SQLITE_DATABASE_URL = "sqlite:///./leads.db"
 engine = create_engine(SQLITE_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-Base.metadata.create_all(bind=engine)
--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 from app.api.api import app
 import uvicorn
 
+from app.database import Base, engine
+
 if __name__ == "__main__":
+    Base.metadata.create_all(bind=engine)
     uvicorn.run(app, host="0.0.0.0", port=8000)
